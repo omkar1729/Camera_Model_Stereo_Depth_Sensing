50cc047177810208d7ccce33d3aa21c4528bfd46
SarthakeChoudhary <sarthak.choudhary24@gmail.com>
2020-03-02 17:59:52 -0700

    reverted to old t1

--- code/task_1/t1.py
+++ code/task_1/t1.py
@@ -2,9 +2,15 @@ import cv2
 import numpy as np
 
 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*9,3), np.float32)
+objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+# print(objp)
+
+
+
 d3=[]
-ip_l=[]
-ip_r=[]
+ip=[]
 d3_points = []
 for i in range(6):
     for j in range(9):
@@ -12,9 +18,9 @@ for i in range(6):
 
 d3_points = np.asarray(d3_points,np.float32)
 
-#print(d3_points)
-str_left = '/home/omkar/PycharmProjects/Perception-Project-2/images/task_1/left_{}.png'
-str_right = '/home/omkar/PycharmProjects/Perception-Project-2/images/task_1/right_{}.png'
+print(d3_points)
+str_left = '/home/sarthake/project_2a/images/task_1/left_{}.png'
+str_right = '/home/sarthake/project_2a/images/task_1/right_{}.png'
 df = (str_left.format(1))
 
 for i in range(11):
@@ -25,13 +31,11 @@ for i in range(11):
     ret_r, point_cor_r = cv2.findChessboardCorners(right, (9, 6))
 
     # print(point_cor[:,:,0])
-    # cv2.drawChessboardCorners(left, (9, 6), point_cor_l, ret_l)
-    # cv2.drawChessboardCorners(right, (9, 6), point_cor_r, ret_r)
+    cv2.drawChessboardCorners(left, (9, 6), point_cor_l, ret_l)
+    cv2.drawChessboardCorners(right, (9, 6), point_cor_r, ret_r)
 
-    d3.append(d3_points)
-    ip_l.append(point_cor_l)
-    ip_r.append(point_cor_r)
-
+    d3.append(objp)
+    ip.append(point_cor_l)
 
 
     cv2.imshow('window_left', left)
@@ -39,33 +43,21 @@ for i in range(11):
     cv2.waitKey(100)
     cv2.destroyAllWindows()
 
-print(point_cor_l.shape, d3_points.shape, left.shape[:-1])
-retl, mtxl, distl, rvecsl, tvecsl = cv2.calibrateCamera(d3, ip_l, left.shape[:-1], None, None,flags=cv2.CALIB_RATIONAL_MODEL)
-retr, mtxr, distr, rvecsr, tvecsr = cv2.calibrateCamera(d3, ip_r, right.shape[:-1], None, None,flags=cv2.CALIB_RATIONAL_MODEL)
-
+print(point_cor_l.shape, d3_points.shape,objp.shape, left.shape[:-1])
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(d3, ip, left.shape[:-1], None, None)
 
 
 h,  w = left.shape[:-1]
 
-newcameramtx_l, roi_l=cv2.getOptimalNewCameraMatrix(mtxl,distl,(w,h),1, (w,h))
-newcameramtx_r, roi_r=cv2.getOptimalNewCameraMatrix(mtxr,distr,(w,h),1, (w,h))
-
-
-print(mtxl,mtxr)
-print(roi_l,roi_r)
-np.savetxt('/home/omkar/PycharmProjects/Perception-Project-2/parameters/intrinsic_l.csv', mtxl, delimiter = ',')
-np.savetxt('/home/omkar/PycharmProjects/Perception-Project-2/parameters/intrinsic_r.csv', mtxr, delimiter = ',')
-
+print(mtx)
+# print(rvecs,tvecs)
+np.savetxt('/home/sarthake/project_2a/parameters/intrinsic.csv', mtx, delimiter = ',')
 
 left = cv2.imread(str_left.format(0))
-mapx, mapy = cv2.initUndistortRectifyMap(mtxl, distl, None, newcameramtx_l, (w,h), 5)
+mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, None, None, (w,h), 5)
 dst = cv2.remap(left, mapx, mapy, cv2.INTER_LINEAR)
-x,y,w,h = roi_l
-dst = dst[y:y+h, x:x+w]
 # print(dst.shape)
-np.savetxt('/home/omkar/PycharmProjects/Perception-Project-2/parameters/distortion_l.csv', distl, delimiter = ',')
-np.savetxt('/home/omkar/PycharmProjects/Perception-Project-2/parameters/distortion_r.csv', distr, delimiter = ',')
-
+np.savetxt('/home/sarthake/project_2a/parameters/distortion1.csv', dist, delimiter = ',')
 # with open('/home/sarthake/project_2a/parameters/distortion.csv', 'w') as outfile:
 #     # outfile.write('# Array shape: {0}\n'.format(dst.shape))
 #
@@ -77,4 +69,3 @@ np.savetxt('/home/omkar/PycharmProjects/Perception-Project-2/parameters/distorti
 cv2.imshow('calibresult', dst)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
